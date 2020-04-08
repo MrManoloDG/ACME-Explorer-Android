f5a41d4bb96604b35d7445fad87a252a92f0cccb
Manuel <manueldiazgil96@hotmail.com>
2020-04-08 10:42:41 +0200

    Adding Filter And Columns TODO: FilterActivity and logic

--- src/main/java/com/example/acme_explorer/ListTripsActivity.java
+++ src/main/java/com/example/acme_explorer/ListTripsActivity.java
@@ -2,9 +2,13 @@ package com.example.acme_explorer;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 import com.example.acme_explorer.adapters.TripAdapter;
 import com.example.acme_explorer.entity.Trip;
@@ -15,6 +19,8 @@ public class ListTripsActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     ArrayList<Trip> trips;
+    LinearLayout filter;
+    Switch columns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +31,25 @@ public class ListTripsActivity extends AppCompatActivity {
         TripAdapter adapter = new TripAdapter(trips);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new GridLayoutManager(this,1));
+
+        columns = findViewById(R.id.columns);
+        columns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(columns.isChecked()){
+                    recyclerView.setLayoutManager(new GridLayoutManager(getBaseContext(),2));
+                } else {
+                    recyclerView.setLayoutManager(new GridLayoutManager(getBaseContext(),1));
+                }
+            }
+        });
+
+        filter = findViewById(R.id.layoutFilter);
+        filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO : Filter View saving in shared preferences
+            }
+        });
     }
 }
